c3e4e1d4b617ee20c9316d736cf98854774546f0
이상화 <sanghwa1215@naver.com>
2025-01-20 12:37:51 +0900

    feat: 프로젝트 초기설정

---
+++ src/main/java/com/example/posturepro/PostureProApplication.java
@@ -0,0 +1,13 @@
+package com.example.posturepro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostureProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostureProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/posturepro/PostureProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.posturepro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostureProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
