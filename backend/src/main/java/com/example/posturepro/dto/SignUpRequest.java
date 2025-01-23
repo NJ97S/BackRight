ef237b24509bd599b9e14e7859f03aaaa7e91334
이상화 <sanghwa1215@naver.com>
2025-01-23 17:53:10 +0900

    feat: SignUpRequest DTO 클래스 생성

---
+++ backend/src/main/java/com/example/posturepro/dto/SignUpRequest.java
@@ -0,0 +1,27 @@
+package com.example.posturepro.dto;
+
+import java.time.LocalDate;
+
+import com.example.posturepro.domain.member.Gender;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class SignUpRequest {
+
+	@NotBlank(message = "이름은 필수 입력 항목입니다.")
+	private String name;
+
+	@NotBlank(message = "닉네임은 필수 입력 항목입니다.")
+	private String nickname;
+
+	@NotNull(message = "생년월일은 필수 입력 항목입니다.")
+	private LocalDate birthDate;
+
+	@NotNull(message = "성별은 필수 입력 항목입니다.")
+	private Gender gender;
+
+}
