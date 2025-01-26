98d73ac70ff2a0a3214009e681edd28540fc887d
SONAMJU <skawn1228@gmail.com>
2025-01-26 16:49:09 +0900

    feat: layout에 SideBar 컴포넌트 추가

--- frontend/src/AppLayout.tsx
+++ frontend/src/AppLayout.tsx
@@ -1,10 +1,18 @@
 import { Outlet } from "react-router-dom";
+
 import Header from "./components/common/Header/Header";
+import SideBar from "./components/common/SideBar/SideBar";
+
+import * as S from "./AppLayoutStyle";
 
 const AppLayout = () => (
   <>
     <Header />
-    <Outlet />
+
+    <S.BodyContainer>
+      <SideBar />
+      <Outlet />
+    </S.BodyContainer>
   </>
 );
 
