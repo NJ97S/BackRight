71fe62cc06cbbff02c75cabcffeeb77510aab44f
SONAMJU <skawn1228@gmail.com>
2025-02-06 00:29:34 +0900

    feat: 사이드바 컴포넌트 생성

--- frontend/src/components/common/Header/Header.tsx
+++ frontend/src/components/common/Header/Header.tsx
@@ -3,11 +3,15 @@ import * as S from "./HeaderStyle";
 import bellIcon from "../../../assets/icons/bell.svg";
 import hamburgerIcon from "../../../assets/icons/hamburger.svg";
 
-const Header = () => (
+interface HeaderProps {
+  onToggleSideBar: () => void;
+}
+
+const Header = ({ onToggleSideBar }: HeaderProps) => (
   <S.HeaderContainer>
-    <S.IconLink to="#">
+    <S.IconButton onClick={onToggleSideBar}>
       <S.Icon src={hamburgerIcon} alt="메뉴 아이콘" />
-    </S.IconLink>
+    </S.IconButton>
 
     <S.SideContainer>
       <S.IconLink to="#">
