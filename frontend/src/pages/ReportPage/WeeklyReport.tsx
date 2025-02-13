1256e6b4921c9efa3e2eb69e205dfce9e5eaa75c
허건 <ssgeonu@gmail.com>
2025-02-13 09:55:13 +0900

    style: React.FC 를 props를 직접 지정하게끔 수정

--- frontend/src/pages/ReportPage/WeeklyReport.tsx
+++ frontend/src/pages/ReportPage/WeeklyReport.tsx
@@ -1,19 +1,21 @@
-import type React from "react";
 import CalendarSection from "../../components/Calendar/Calendar";
 import PostureWarningSection from "../../components/PostureWarning/PostureWarning";
 import AveragePostureTime from "../../components/common/charts/AveragePostureTime";
 
-const WeeklyReport: React.FC = () => {
-  const weeklyData = [45, 55, 25, 48, 44, 5, 13];
-  const weekLabels = ["월", "화", "수", "목", "금", "토", "일"];
+interface WeeklyReportProps {
+  weeklyData?: number[];
+  weekLabels?: string[];
+}
 
-  return (
-    <>
-      <PostureWarningSection />
-      <CalendarSection />
-      <AveragePostureTime data={weeklyData} labels={weekLabels} />
-    </>
-  );
-};
+const WeeklyReport = ({
+  weeklyData = [45, 55, 25, 48, 44, 5, 13],
+  weekLabels = ["월", "화", "수", "목", "금", "토", "일"],
+}: WeeklyReportProps) => (
+  <>
+    <PostureWarningSection />
+    <CalendarSection />
+    <AveragePostureTime data={weeklyData} labels={weekLabels} />
+  </>
+);
 
 export default WeeklyReport;
