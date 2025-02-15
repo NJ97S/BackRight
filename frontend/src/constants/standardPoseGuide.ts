197bc7ecc60a9b308ea6c88e924bd4f5de43d463
SONAMJU <skawn1228@gmail.com>
2025-02-15 23:22:32 +0900

    fix: 이미지 경로 수정

--- frontend/src/constants/standardPoseGuide.ts
+++ frontend/src/constants/standardPoseGuide.ts
@@ -1,26 +1,26 @@
 const STANDARD_POSE_GUIDE = [
   {
-    src: "/test-pose-guide.webp",
+    src: "/images/test-pose-guide.webp",
     description:
       "웹캠이 얼굴과 상반신을 잘 인식할 수 있도록\n위치를 설정해주세요.",
   },
   {
-    src: "/test-pose-guide.webp",
+    src: "/images/test-pose-guide.webp",
     description:
       "발바닥이 바닥에 닿도록 의자 높이를 조정하고,\n무릎은 90도를 유지하세요.",
   },
   {
-    src: "/test-pose-guide.webp",
+    src: "/images/test-pose-guide.webp",
     description:
       "엉덩이를 의자 깊숙이 넣고\n허리를 등받이에 기대어 곧게 펴세요.",
   },
   {
-    src: "/test-pose-guide.webp",
+    src: "/images/test-pose-guide.webp",
     description:
       "어깨에 힘을 빼고 팔꿈치가 90도가 되도록\n키보드와 마우스를 조정하세요.",
   },
   {
-    src: "/test-pose-guide.webp",
+    src: "/images/test-pose-guide.webp",
     description:
       "모니터 상단을 눈높이에 맞춰\n목이 과도하게 숙여지지 않도록 하세요.",
   },
