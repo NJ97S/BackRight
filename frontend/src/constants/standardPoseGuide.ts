f1ce68964931e86e0ab2502eb71d30d7ba0a6527
SONAMJU <skawn1228@gmail.com>
2025-02-16 23:17:21 +0900

    feat: 바른 자세 가이드에 이미지 추가

--- frontend/src/constants/standardPoseGuide.ts
+++ frontend/src/constants/standardPoseGuide.ts
@@ -1,28 +1,28 @@
 const STANDARD_POSE_GUIDE = [
   {
-    src: "/images/test-pose-guide.webp",
-    description:
-      "웹캠이 얼굴과 상반신을 잘 인식할 수 있도록\n위치를 설정해주세요.",
+    src1: "/images/poseGuide/1-1.webp",
+    src2: "/images/poseGuide/1-2.webp",
+    description: "무릎, 발목이 90도가 되도록 의자를 조절하세요",
   },
   {
-    src: "/images/test-pose-guide.webp",
-    description:
-      "발바닥이 바닥에 닿도록 의자 높이를 조정하고,\n무릎은 90도를 유지하세요.",
+    src1: "/images/poseGuide/2.webp",
+    src2: "/images/poseGuide/2.webp",
+    description: "어깨 넓이로 다리를 벌려주세요",
   },
   {
-    src: "/images/test-pose-guide.webp",
-    description:
-      "엉덩이를 의자 깊숙이 넣고\n허리를 등받이에 기대어 곧게 펴세요.",
+    src1: "/images/poseGuide/3-1.webp",
+    src2: "/images/poseGuide/3-2.webp",
+    description: "골반을 앞으로 젖혀 허리를 최대한 펴주세요",
   },
   {
-    src: "/images/test-pose-guide.webp",
-    description:
-      "어깨에 힘을 빼고 팔꿈치가 90도가 되도록\n키보드와 마우스를 조정하세요.",
+    src1: "/images/poseGuide/4-1.webp",
+    src2: "/images/poseGuide/4-2.webp",
+    description: "허리에 힘이 들어가지 않을 때까지 천천히 되돌아오세요",
   },
   {
-    src: "/images/test-pose-guide.webp",
-    description:
-      "모니터 상단을 눈높이에 맞춰\n목이 과도하게 숙여지지 않도록 하세요.",
+    src1: "/images/poseGuide/5-1.webp",
+    src2: "/images/poseGuide/5-2.webp",
+    description: "기지개를 핀 후, 천천히 팔을 내리고 책상에 올려주세요.",
   },
 ] as const;
 
