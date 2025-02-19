82e267e82c73796a04b26554e5d1b3db6ba9eb34
소남주 <skawn1228@gmail.com>
2025-02-19 11:02:07 +0900

    feat: 로그아웃 구현

--- frontend/src/components/common/SideBar/SideBarStyle.ts
+++ frontend/src/components/common/SideBar/SideBarStyle.ts
@@ -42,6 +42,17 @@ export const IconLink = styled(Link).withConfig({
   font-weight: 600;
 `;
 
+export const IconButton = styled.button.withConfig({
+  shouldForwardProp: (prop) => prop !== "isExpanded",
+})<SideBarProps>`
+  position: relative;
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  width: 100%;
+  font-weight: 600;
+`;
+
 export const Icon = styled.img`
   width: 2rem;
   height: 2rem;
